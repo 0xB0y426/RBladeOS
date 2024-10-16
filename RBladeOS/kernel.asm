a6b51cb1f57717c96b7de240ab62f8a00374c080
R4idB0y <132174647+R4idB0Y@users.noreply.github.com>
2024-10-16 01:49:45 +0000

    Add files via upload

--- RBladeOS/kernel.asm
+++ RBladeOS/kernel.asm
@@ -1,20 +1,27 @@
-; Writed by R4idB0y
-; Kernel for RBladeOS
-; Copyright (C) 2024 - R4idB0Y
-
 [BITS 16]
 [ORG 0x1000]
 
 start:
     cli
-    mov si, msg
-    call print_string
+    call set_video_mode
+    call print_interface
     call print_newline
     call shell
     jmp $
 
+set_video_mode:
+    mov ax, 0x03
+    int 0x10
+    mov ah, 0x0B
+    mov bh, 0x00
+    mov bl, 0x01
+    int 0x10
+    ret
+
 print_string:
     mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x1F
 .next_char:
     lodsb
     cmp al, 0
@@ -32,6 +39,15 @@ print_newline:
     int 0x10
     ret
 
+print_interface:
+    mov si, header
+    call print_string
+    call print_newline
+    mov si, menu
+    call print_string
+    call print_newline
+    ret
+
 shell:
     mov si, prompt
     call print_string
@@ -45,82 +61,106 @@ read_command:
     xor cx, cx
 .read_loop:
     mov ah, 0x00
-    int 0x16             
-    cmp al, 0x0D         
+    int 0x16
+    cmp al, 0x0D
     je .done_read
-    cmp al, 0x08         
+    cmp al, 0x08
     je .handle_backspace
-    cmp cx, 150           
-    jge .done_read  
-    stosb                 
+    cmp cx, 150
+    jge .done_read
+    stosb
     mov ah, 0x0E
-    int 0x10              
+    mov bl, 0x1F
+    int 0x10
     inc cx
     jmp .read_loop
 
 .handle_backspace:
     cmp di, command_buffer
-    je .read_loop           
-    dec di                 
-    dec cx           
+    je .read_loop
+    dec di
+    dec cx
     mov ah, 0x0E
-    mov al, 0x08           
+    mov al, 0x08
     int 0x10
-    mov al, ' '             
+    mov al, ' '
     int 0x10
-    mov al, 0x08         
+    mov al, 0x08
     int 0x10
     jmp .read_loop
 
 .done_read:
-    mov byte [di], 0     
+    mov byte [di], 0
     ret
 
 execute_command:
     mov si, command_buffer
-    cmp byte [si], 'e'
-    je do_echo
-    cmp byte [si], 'c'
-    cmp byte [si + 1], 'l'
-    cmp byte [si + 2], 'e'
-    cmp byte [si + 3], 'a'
-    cmp byte [si + 4], 'r'
+    cmp byte [si], 'P'
+    je check_print
+    cmp byte [si], 'C'
+    je check_cls
+    cmp byte [si], 'R'
+    je check_reboot
+    cmp byte [si], 'S'
+    je check_shutdown
+    cmp byte [si], 'L'
+    je check_loop
+    jmp unknown_command
+
+check_print:
+    cmp byte [si + 1], 'R'
+    je do_print
+    jmp unknown_command
+
+check_cls:
+    cmp byte [si + 1], 'L'
     je do_cls
-    cmp byte [si], 'h'
-    cmp byte [si + 1], 'e'
-    cmp byte [si + 2], 'l'
-    cmp byte [si + 3], 'p'
-    je help_command
-    cmp byte [si], 'r'
-    cmp byte [si + 1], 'e'
-    cmp byte [si + 2], 'b'
-    cmp byte [si + 3], 'o'
-    cmp byte [si + 4], 'o'
-    cmp byte [si + 5], 't'
+    jmp unknown_command
+
+check_reboot:
+    cmp byte [si + 1], 'E'
     je do_reboot
-    cmp byte [si], 's'
-    cmp byte [si + 1], 'h'
-    cmp byte [si + 2], 'u'
-    cmp byte [si + 3], 't'
+    jmp unknown_command
+
+check_shutdown:
+    cmp byte [si + 1], 'H'
     je do_shutdown
     jmp unknown_command
 
-do_echo:
+check_loop:
+    cmp byte [si + 1], 'O'
+    je do_loop
+    jmp unknown_command
+
+do_loop:
+    mov si, command_buffer + 6
+    cmp byte [si], 0
+    je .done
+.loop:
+    push si
+    call print_string
+    call print_newline
+    pop si
+    jmp .loop
+.done:
+    ret
+
+do_print:
     mov si, command_buffer + 5
     call print_string
     call print_newline
     ret
 
 do_reboot:
-    db 0x0ea 
-    dw 0x0000 
-    dw 0xffff 
+    db 0x0EA
+    dw 0x0000
+    dw 0xFFFF
 
 do_shutdown:
-    mov ax, 0x5307    
-    mov bx, 0x0001    
-    mov cx, 0x0003    
-    int 0x15         
+    mov ax, 0x5307
+    mov bx, 0x0001
+    mov cx, 0x0003
+    int 0x15
     ret
 
 do_cls:
@@ -136,15 +176,8 @@ unknown_command:
     call print_newline
     ret
 
-help_command:
-    mov si, help_msg
-    call print_string
-    call print_newline
-    ret
-
-msg db 'RBladeOS v0.1', 0
-prompt db '> ', 0
-command_buffer db 150 dup(0)  
+header db '============================ RBladeOS v0.2 =====================================', 0
+menu db 'Available Commands: PRINT, CLS, REBOOT, SHUT, LOOP', 0
+prompt db '[RBLADEOS] > ', 0
+command_buffer db 150 dup(0)
 unknown_msg db 'Unknown command', 0
-help_msg db 'Manual for RBladeOS: echo, clear, help, reboot, shut', 0
-dir_msg db 'Listing files:', 0
